cd4f8466f75707f5d67a87904b38f0a0eb1f4a54
gbarcomu <gbarcomu@gmail.com>
2020-07-28 12:27:05 +0200

    Improvements

--- VaultSignInteraction/index.js
+++ VaultSignInteraction/index.js
@@ -1,20 +1,24 @@
+const ETHEREUM_URL = 'https://rinkeby.infura.io/v3/f2a8581c640340758bead17199084148';
+const VAULT_URL = 'http://127.0.0.1:8200';
+
 const Web3 = require('web3');
 const rp = require('request-promise');
-const web3 = new Web3('https://rinkeby.infura.io/v3/f2a8581c640340758bead17199084148');
-var Tx = require('ethereumjs-tx').Transaction;
-const queryString = require('query-string');
+const web3 = new Web3(ETHEREUM_URL);
+const Tx = require('ethereumjs-tx').Transaction;
 
+const ETH_ACCOUNT = '0xc8dfCA661A53bC05EC1BC76d20Ba77C34F8facAb';
+const ETH_PRIV_KEY = '2659d295cf455bc033e5b5ec59afc67057425af8a71a694a5f59ad0e6b333f0c';
+const ETH_NETWORK = 'rinkeby';
 
 (async function () {
 
     const privateKey = Buffer.from(
-        '2659d295cf455bc033e5b5ec59afc67057425af8a71a694a5f59ad0e6b333f0c',
+        ETH_PRIV_KEY,
         'hex',
     )
 
-    const tnonce = await web3.eth.getTransactionCount('0xc8dfCA661A53bC05EC1BC76d20Ba77C34F8facAb')
+    const tnonce = await web3.eth.getTransactionCount(ETH_ACCOUNT)
     const tnonceHex = `0x${tnonce.toString(16)}`;
-
 
     const txParams = {
         nonce: tnonceHex,
@@ -28,37 +32,20 @@ const queryString = require('query-string');
         s: ''
     }
 
-    const txParams2 = {
-        nonce: tnonceHex,
-        gasPrice: '0x161E70F600',
-        gas: '0x989680',
-        to: '0xf17f52151EbEF6C7334FAD080c5704D77216b732',
-        value: '0x1',
-        input: '0x',
-    }
-
-
-    // The second parameter is not necessary if these values are used
-    tx2 = new Tx(txParams2, { 'chain': 'rinkeby' })
-    tx2.sign(privateKey)
-    const serializedTx2 = tx2.serialize();
-
-    console.log(tx2.toJSON())
-
-
-
     const urlEncodedTx = urlEncoder(txParams)
 
-    console.log(urlEncodedTx);
+    console.log(`URL encoded transaction: ${urlEncodedTx}`);
 
     try {
-        const foo = await getVault(urlEncodedTx);
-        console.log(foo.data.result);
-        const tx = new Tx(JSON.parse(foo.data.result), { 'chain': 'rinkeby' });
-        console.log(tx.toJSON())
+        const signedTx = await getVault(urlEncodedTx);
+
+        console.log(`Transaction with signature: ${signedTx.data.result}`)
+
+        const tx = new Tx(JSON.parse(signedTx.data.result), { 'chain': ETH_NETWORK });
         const serializedTx = tx.serialize();
         const res = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
-        console.log(res);
+        console.log("View on Etherscan:");
+        console.log(`https://rinkeby.etherscan.io/tx/${res.transactionHash}`);
     }
     catch (err) {
         console.error(err);
@@ -73,13 +60,13 @@ const getVault = (urlEncodedTx) => {
             headers: {
                 'X-Vault-Token': 'root'
             },
-            uri: `http://127.0.0.1:8200/v1/signTx/ethKeypedro?tx=${urlEncodedTx}`,
+            uri: `${VAULT_URL}/v1/signTx/ethKeypedro?tx=${urlEncodedTx}`,
             json: true,
         };
 
         rp(options)
             .then(res => {
-                console.log("Bien")
+                console.log("Transaction successfully sent")
                 resolve(res);
             })
             .catch(err => {
